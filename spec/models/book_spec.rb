ce7f2657a7008bd0e434af4e3a15235422acc2b9
Robert Farese <robertfarese@yahoo.com>
2016-06-28 14:22:48 -0400

    added book model initial specs

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  it { should have_valid(:title).when("Some Book Title", "Book Title Stuff") }
+  it { should_not have_valid(:title).when(nil, "") }
 
-  it "must have a title, author, description"
+  it { should have_valid(:author).when("Mark Smith", "Bobby LastName")}
+  it { should_not have_valid(:author).when(nil, "") }
 
-  it "can have an optional book cover"
+  it { should have_valid(:description).when("This is a description and it is a really good one at that.") }
+  it { should_not have_valid(:description).when(nil, "") }
+
+  it { should have_valid(:book_cover_photo).when(nil, "", "http://www.someurl.com") }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:first_name).when("Bobby", "Sarah" )}
+  it { should_not have_valid(:first_name).when(nil, "") }
+
+  it { should have_valid(:last_name).when("Farese", "Smith") }
+  it { should_not have_valid(:last_name).when(nil, "") }
+
+  it { should have_valid(:email).when("email@yahoo.com", "rfarese@gmail.com") }
+  it { should_not have_valid(:email).when(nil, "", "userMan", "uManDude.com", "@com") }
+
+  it 'has a matching password confirmation for the password' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
 end
