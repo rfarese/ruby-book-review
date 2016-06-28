35c627daedda8023bd3c8d6fcd3976327a717867
Robert Farese <robertfarese@yahoo.com>
2016-06-28 16:12:13 -0400

    completed user sign in requirements

---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "User creates a new account", type: :feature do
+  scenario "User sees link to sign in if the user is unauthenticated" do
+    user = FactoryGirl.create(:user)
+    visit root_path
+
+    expect(page).to have_content("Sign In")
+  end
+
+  scenario "a returning user provides all valid information when signing in" do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link "Sign In"
+    fill_in 'Email', with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content("Sign Out")
+    expect(page).to_not have_content("Sign In")
+  end
+
+  scenario "user doesn't provide valid credentials" do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link "Sign In"
+    fill_in 'Email', with: "youremail"
+    fill_in "Password", with: "mypassword"
+    click_button "Log in"
+
+    expect(page).to have_content("Invalid Email or password")
+    expect(page).to have_content("Sign In")
+    expect(page).to_not have_content("Sign Out")
+  end
+end
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-# As a prospective user
-# I want to create an account
-# So that I can post lakes and review them
-
-# Acceptance Criteria:
-# There is an option to create a new user account on the top of every page (in the nav bar) if I'm not already authenticated
-# I must specify a valid and unique email address
-# I must specify a unique username
-# I must provide a unique password
-# I can provide an optional user profile picture
-# If I've entered valid information, I receive an onscreen message that my account has been created
-# If I didn't enter valid information, I receive an error message and I am left unauthenticated
-# If I'm already authenticated, I don't have the option to create a new user account
-
 RSpec.feature "User creates a new account", type: :feature do
   scenario "User sees link to create a new account" do
     visit root_path
@@ -22,8 +8,6 @@ RSpec.feature "User creates a new account", type: :feature do
   end
 
   scenario "user provides valid and required information" do
-    # check for messages
-    # check for the lack of a "Sign Up"
     visit root_path
     click_link "Sign Up"
     fill_in "user_first_name", with: "Jon"
@@ -65,7 +49,7 @@ RSpec.feature "User creates a new account", type: :feature do
     expect(page).to have_content("Email is invalid")
   end
 
-  scenario "User password doesn't match with their password confirmation" do 
+  scenario "User password doesn't match with their password confirmation" do
   visit root_path
   click_link "Sign Up"
   fill_in "user_first_name", with: "Jon"
