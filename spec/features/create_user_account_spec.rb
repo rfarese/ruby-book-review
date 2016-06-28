e6efc4ccc1388a132580fc023d0f345014acc9e6
Robert Farese <robertfarese@yahoo.com>
2016-06-28 15:51:47 -0400

    completed user sign up

--- spec/features/create_user_account_spec.rb
+++ spec/features/create_user_account_spec.rb
@@ -16,20 +16,65 @@ require 'rails_helper'
 
 RSpec.feature "User creates a new account", type: :feature do
   scenario "User sees link to create a new account" do
-    visit "/"
+    visit root_path
 
     expect(page).to have_content("Sign Up")
   end
 
-  scenario "User successfully creates a new account"
+  scenario "user provides valid and required information" do
     # check for messages
     # check for the lack of a "Sign Up"
-  scenario "User doesn't provide a valid email address"
-    # check for uniqueness
-    # check for invalid email syntax
-  scenario "User doesn't provide a valid password"
-    # check to see if password matches password confirmation
-    # check for uniqueness
-  scenario "User doesn't provide a profile picture"
-    # check to see if the user is still authenticated with the absence of a profile picture
+    visit root_path
+    click_link "Sign Up"
+    fill_in "user_first_name", with: "Jon"
+    fill_in "user_last_name", with: "Smith"
+    fill_in "Email", with: 'user@example.com'
+    fill_in "user_password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("Welcome! You have signed up successfully.")
+    expect(page).to have_content("Sign Out")
+    expect(page).to_not have_content("Sign In")
+    expect(page).to_not have_content("Sign Up")
+  end
+
+  scenario "User doesn't provide any email address" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "user_first_name", with: "Jon"
+    fill_in "user_last_name", with: "Smith"
+    fill_in "Email", with: ''
+    fill_in "user_password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("Email can't be blank")
+  end
+
+  scenario "User doesn't provide a valid email address" do
+    visit root_path
+    click_link "Sign Up"
+    fill_in "user_first_name", with: "Jon"
+    fill_in "user_last_name", with: "Smith"
+    fill_in "Email", with: 'user'
+    fill_in "user_password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    expect(page).to have_content("Email is invalid")
+  end
+
+  scenario "User password doesn't match with their password confirmation" do 
+  visit root_path
+  click_link "Sign Up"
+  fill_in "user_first_name", with: "Jon"
+  fill_in "user_last_name", with: "Smith"
+  fill_in "Email", with: 'user@example.com'
+  fill_in "user_password", with: "password"
+  fill_in "Password confirmation", with: "differentPassword"
+  click_button "Sign up"
+
+  expect(page).to have_content("Password confirmation doesn't match Password")
+  end
 end
