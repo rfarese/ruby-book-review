c788d76040d09cac10e338dc5fd4601b1ec08e23
Robert Farese <robertfarese@yahoo.com>
2016-07-03 12:23:39 -0400

    added tests and functionality for editing a vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -21,6 +21,28 @@ class VotesController < ApplicationController
     redirect_to book_path(@book)
   end
 
+  def do_user_ids_match_update(review, vote)
+    if current_user.id != review.user_id
+      vote.update(vote_params)
+      flash[:notice] = "You've successfully updated your vote"
+    else
+      flash[:notice] = "You can't vote for your own review"
+    end
+  end
+
+  def update
+    @review = Review.find(params[:review_id])
+    @book = Book.where(id: @review.book_id).first
+    @vote = Vote.where(review_id: params[:review_id]).first
+
+    if user_signed_in?
+      do_user_ids_match_update(@review, @vote)
+    else
+      flash[:notice] = "You must be signed in to vote"
+    end
+    redirect_to book_path(@book)
+  end
+
   private
   def vote_params
     { review_id: params[:review_id], user_id: current_user.id, up_vote: params[:up_vote], down_vote: params[:down_vote] }
---
+++ spec/features/votes/edit_vote_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.feature "User edits a vote;", type: :feature do
+
+  def sign_in(user)
+    visit root_path
+    click_link "Sign In"
+    fill_in 'Email', with: user.email
+    fill_in "Password", with: "password"
+    click_button "Log in"
+  end
+
+  scenario "authenticated user successfully changes a vote" do
+    review = FactoryGirl.create(:review)
+    book = Book.where(id: review.book_id).first
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    click_link book.title
+    click_link "Up Vote"
+    click_link "Down Vote"
+    vote = Vote.where(user_id: user.id).first
+
+    expect(vote.up_vote).to eq(false)
+    expect(vote.down_vote).to eq(true)
+    expect(vote.review_id).to eq(review.id)
+  end
+
+  scenario "An unauthenticated user can not change a vote" do
+    vote = FactoryGirl.create(:vote)
+    review = Review.where(id: vote.review_id).first
+    book = Book.where(id: review.book_id).first
+    visit root_path
+    click_link book.title
+    click_link "Down Vote"
+
+    expect(page).to have_content("You must be signed in to vote")
+  end
+
+  scenario "An authenticated user can not change a vote for a review they've created" do
+    vote = FactoryGirl.create(:vote)
+    review = Review.where(id: vote.review_id).first
+    book = Book.where(id: review.book_id).first
+    user = User.where(id: review.user_id).first
+    sign_in(user)
+    click_link book.title
+    click_link "Down Vote"
+
+    expect(page).to have_content("You can't vote for your own review")
+  end
+end
