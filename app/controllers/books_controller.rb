fbed2129f2d9c4d218db9823ec4b5962933af0ec
Robert Farese <robertfarese@yahoo.com>
2016-07-11 07:02:57 -0400

    added tests and functionality for searching for a book by title

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    if params[:search]
+      @books = Book.where("title LIKE ?", params[:search])
+    else
+      @books = Book.all
+    end
   end
 
   def has_user_ranked_book?
---
+++ spec/features/searches/book_search_by_title_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature "User searches for a book by title;", type: :feature do
+  let(:book) { FactoryGirl.create(:book) }
+
+  def visit_root_complete_search
+    visit root_path
+    within("#search-form") do
+      fill_in "search", with: book.title
+    end
+    click_button "Go"
+  end
+
+  scenario "User views the search bar in navigation bar" do
+    visit root_path
+
+    expect(page).to have_css("#search-form")
+  end
+
+  scenario "User views search results (book title & author) on books show page" do
+    book
+    second_book = FactoryGirl.create(:book)
+    visit_root_complete_search
+
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.author)
+    expect(page).to_not have_content(second_book.title)
+  end
+
+  scenario "User navigates from search results page to the books show page" do
+    book
+    visit_root_complete_search
+    click_link book.title
+
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.author)
+    expect(page).to have_content(book.description)
+  end
+end
