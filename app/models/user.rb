66373e70a4c21aacdf594a8738dcea4fd3934989
Robert Farese <robertfarese@yahoo.com>
2016-07-21 19:45:56 -0400

    completed authentication with twitter; need to rewrite the tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,23 @@
+require 'pry'
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:twitter]
   has_many :books
   has_many :reviews
   has_many :votes
-  has_many :ranks 
+  has_many :ranks
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.first_name = auth.info.name
+      user.last_name = auth.info.nickname
+      user.email = "defaultemail@yahoo.com"
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'books#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
   resources :books do
     resources :reviews
   end
