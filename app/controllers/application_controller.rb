8c9c41cb84143f70cc58198048ddda14b0f5dc9d
Robert Farese <robertfarese@yahoo.com>
2016-07-31 09:35:55 -0400

    deleted comments in commanly used files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
@@ -16,20 +14,4 @@ class ApplicationController < ActionController::Base
     user.role == "admin"
   end
   helper_method :admin?
-
-  # def user_voted?(review)
-  #   Vote.where(user_id: current_user.id, review_id: review.id)
-  # end
-  # helper_method :user_voted?
-  #
-  # def user_voting_status(review)
-  #   voting_status = "None"
-  #   if user_signed_in? && has_user_voted?(review)
-  #     vote = Vote.where(user_id: current_user.id, review_id: review.id).first
-  #     voting_status = "Up Voted" if vote.up_vote == true
-  #     voting_status = "Down Voted" if vote.down_vote == true
-  #   end
-  #   voting_status
-  # end
-  # helper_method :user_voting_status
 end
